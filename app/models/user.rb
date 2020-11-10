ac3375f0d161905b844387fad63c96c7bf411cbe
viethuynh <huynhquocviet020998@gmail.com>
2020-11-10 15:12:55 +0700

    Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   enum role: {user: 0, admin: 1}
 
   has_secure_password
+
+  before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_PHONE_REGEX = /\A\d[0-9]{9}\z/.freeze
+  validates :name, presence: true,
+            length: {maximum: Settings.model.user.name_max}
+
+  validates :email, presence: true,
+            length: {maximum: Settings.model.user.email_max},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: true}
+  validates :phone, presence: true,
+            format: {with: VALID_PHONE_REGEX}
+
+  validates :password, presence: true,
+            length: {minimum: Settings.model.user.password_mini},
+            allow_nil: true
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     get "/home", to: "static_pages#home"
     get "/detail", to: "static_pages#detail"
     get "/order", to: "static_pages#order"
+    get "signup", to: "users#new"
+    resources :users, only: %i(new create)
   end
 end
